a56279a22a057d3d42042ad9d56a1d4098b82a80
sfayman <Sergey_Fayman@bmc.com>
2017-02-17 11:19:11 +0200

    implement js page

--- js/js-tool.js
+++ js/js-tool.js
@@ -1,35 +1,12 @@
 (function () {
 	"use strict";
 
-	const ACTION_FAILED_MSG = "<strong>Error!</strong> Something went wrong. Please fix the css and try again.";
-	const FILE_MODE_CSS = 'css';
+	const ACTION_FAILED_MSG = "<strong>Error!</strong> Something went wrong. Please fix the js and try again.";
+	const FILE_MODE_JS = 'js';
 
-	let fileMode = FILE_MODE_CSS;
+	let fileMode = FILE_MODE_JS;
 
-	const beautifyOptions = {
-		format: {
-			breaks: {
-				afterAtRule: true,
-				afterBlockBegins: true,
-				afterBlockEnds: true,
-				afterComment: true,
-				afterProperty: true,
-				afterRuleBegins: true,
-				afterRuleEnds: true,
-				beforeBlockEnds: true,
-				betweenSelectors: true
-			},
-			indentBy: 4,
-			indentWith: 'space',
-			spaces: {
-				aroundSelectorRelation: true,
-				beforeBlockBegins: true,
-				beforeValue: true
-			},
-			wrapAt: false
-		}
-	};
-
+	
 	function calculateSize() {
 		const sizeOutput = editorOut.getValue().length;
 		const sizeInput = editorInput.getValue().length;
@@ -59,14 +36,14 @@
 	//Start create input editor
 	const editorInput = ace.edit("leftPane");
 	editorInput.setTheme("ace/theme/twilight");
-	editorInput.getSession().setMode("ace/mode/css");
-	const oldInput = DevUtils.retrieveItem("css-input");
-	const example = oldInput ? oldInput : "p {color: red; text-align: center;}";
+	editorInput.getSession().setMode("ace/mode/javascript");
+	const oldInput = DevUtils.retrieveItem("js-input");
+	const example = oldInput ? oldInput : "function getX(){return x;}";
 	editorInput.setValue(example);
 	editorInput.$blockScrolling = Infinity;
 	editorInput.getSession().on('change', function () {
 		const text = editorInput.getValue();
-		DevUtils.storeItem("css-input", text);
+		DevUtils.storeItem("js-input", text);
 	});
 
 	//End create input editor
@@ -74,7 +51,7 @@
 	//Start create output editor
 	const editorOut = ace.edit("rightPane");
 	editorOut.setTheme("ace/theme/twilight");
-	editorOut.getSession().setMode("ace/mode/css");
+	editorOut.getSession().setMode("ace/mode/javascript");
 	editorOut.setReadOnly(true);
 	editorOut.$blockScrolling = Infinity;
 	//End create output editor
@@ -87,19 +64,28 @@
 	});
 
 
-	$('#btn-beautify').on("click", function () {
+	$('#btn-uglify').on("click", function () {
 		try {
 			const input = editorInput.getValue();
-			const output = new CleanCSS(beautifyOptions).minify(input);
-			editorOut.getSession().setMode("ace/mode/css");
-			editorOut.setValue(output.styles);
+			let ast = UglifyJS.parse(input);
+			// compressor needs figure_out_scope too
+			ast.figure_out_scope();
+			const compressor = UglifyJS.Compressor({});
+			ast = ast.transform(compressor);
+			// need to figure out scope again so mangler works optimally
+			ast.figure_out_scope();
+			ast.compute_char_frequency();
+			ast.mangle_names();
+			// get Ugly code back :)
+			const code = ast.print_to_string();
+			editorOut.setValue(code);
 			calculateSize();
-			showMessage("<strong>Success!</strong> CSS has been beautified successfully.", 'success');
-			fileMode = FILE_MODE_CSS;
+			showMessage("<strong>Success!</strong> JS has been uglified successfully.", 'success');
+			fileMode = FILE_MODE_JS;
 		}
 		catch (err) {
 			editorOut.setValue("");
-			showMessage(ACTION_FAILED_MSG, 'danger');
+			showMessage(ACTION_FAILED_MSG + " " + err.message, 'danger');
 		}
 
 
@@ -108,17 +94,20 @@
 	$('#btn-minify').on("click", function () {
 		try {
 			const input = editorInput.getValue();
-			editorOut.getSession().setMode("ace/mode/css");
-			const output = new CleanCSS({}).minify(input);
-			editorOut.setValue(output.styles);
+			let ast = UglifyJS.parse(input);
+			ast.figure_out_scope();
+			const compressor = UglifyJS.Compressor({  });
+			ast = ast.transform(compressor);
+			const output = ast.print_to_string(); // get compressed code
+			editorOut.setValue(output);
 			calculateSize();
-			showMessage("<strong>Success!</strong> CSS has been minified successfully.", 'success');
-			fileMode = FILE_MODE_CSS;
+			showMessage("<strong>Success!</strong> JS has been minified successfully.", 'success');
+			fileMode = FILE_MODE_JS;
 
 		}
 		catch (err) {
 			editorOut.setValue("");
-			showMessage(ACTION_FAILED_MSG, 'danger');
+			showMessage(ACTION_FAILED_MSG + " " + err.message, 'danger');
 		}
 
 
@@ -128,7 +117,7 @@
 		editorOut.selectAll();
 		editorOut.focus();
 		document.execCommand('copy');
-		showMessage("<strong>Success!</strong> CSS has been copied successfully.", 'success');
+		showMessage("<strong>Success!</strong> JS has been copied successfully.", 'success');
 
 	});
 
@@ -138,7 +127,7 @@
 			const text = editorOut.getValue();
 			const blob = new Blob([text], { type: "text/plain;charset=utf-8" });
 			saveAs(blob, "output." + fileMode);
-			showMessage("<strong>Success!</strong> CSS has been saved successfully.", 'success');
+			showMessage("<strong>Success!</strong> JS has been saved successfully.", 'success');
 		}
 		catch (err) {
 			showMessage(ACTION_FAILED_MSG, 'danger');
